89d12d6069385ddc8219b3e500119c286344c307
Hero Li <757937784@qq.com>
2019-03-27 18:30:57 +0800

    identity

---
+++ Mes.Identity.API/Infrastructure/Core/Domain/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mes.Identity.API.Infrastructure.Core.Domain
+{
+    public class ApplicationRole: IdentityRole
+    {
+        [Required]
+        [StringLength(50)]
+        public string DisplayName { get; set; }
+    }
+}
---
+++ Mes.Identity.API/Infrastructure/Core/Domain/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mes.Identity.API.Infrastructure.Core.Domain
+{
+    public class ApplicationUser:IdentityUser
+    {
+    }
+}
